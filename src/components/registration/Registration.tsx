b8b5448579fbd00e56f8d88699231e9de20b6b6a
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-06 02:36:47 +0300

    add error validation

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -23,18 +23,22 @@ const Registration = () => {
     }
     const registrationOnClick = () => {
         setLoader('sending...')
-        registrationApi.registrationUser(emailState, passwordState)
-            .then(() => {
-                setErrorMessage('')
-                setRegistrationSuccess(true)
-            })
-            .catch((error: AxiosError) => {
-                setErrorMessage(error.response?.data.error)
-            })
-            .finally(() => {
-                setLoader('')
-            })
-
+        if (passwordState !== confirmPassState) {
+            setErrorMessage('passwords don\'t match!')
+            setLoader('')
+        } else {
+            registrationApi.registrationUser(emailState, passwordState)
+                .then(() => {
+                    setErrorMessage('')
+                    setRegistrationSuccess(true)
+                })
+                .catch((error: AxiosError) => {
+                    setErrorMessage(error.response?.data.error)
+                })
+                .finally(() => {
+                    setLoader('')
+                })
+        }
     }
     if (registrationSuccess) {
         return <Navigate to='/profile'/>
@@ -44,17 +48,17 @@ const Registration = () => {
             <h1>It-incubator</h1>
             <h3>Sign up</h3>
             <div className={styles.inputsBlock}>
-                <input type='text'
+                <input type='email'
                        placeholder='Email'
                        value={emailState}
                        onChange={emailHandler}/>
 
-                <input type='text'
+                <input type='password'
                        placeholder='Password'
                        value={passwordState}
                        onChange={passwordHandler}/>
 
-                <input type='text'
+                <input type='password'
                        placeholder='Confirm password'
                        value={confirmPassState}
                        onChange={confirmPassHandler}/>
