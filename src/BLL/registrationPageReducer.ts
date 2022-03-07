f667a21ce815b5496eafccff866d4734c174d5d2
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-07 10:07:06 +0300

    add tests for registrationpageReducer

--- src/BLL/registrationPageReducer.ts
+++ src/BLL/registrationPageReducer.ts
@@ -1,5 +1,34 @@
-
-
-export const registrationPageReducer = () => {
-
+const initialState = {
+    successMessage: false,
+    loader: false,
+    error: ''
 }
+
+export const registrationPageReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case "registration/SET_LOADER":
+            return {...state, loader: action.value}
+        case "registration/SET_SUCCESS_MESSAGE":
+            return {...state, successMessage: action.value}
+        case "registration/SET_ERROR":
+            return {...state, error: action.errorText}
+        default:
+            return state
+    }
+}
+// action creators
+export const setSuccessMessageAC = (value: boolean) => ({type: 'registration/SET_SUCCESS_MESSAGE', value} as const)
+export const setLoaderAC = (value: boolean) => ({type: 'registration/SET_LOADER', value} as const)
+export const setErrorActionAC = (errorText: string) => ({
+    type: 'registration/SET_ERROR',
+    errorText
+} as const)
+
+//thunks
+
+//types
+type SetSuccessMessageActionType = ReturnType<typeof setSuccessMessageAC>
+type LoaderActionType = ReturnType<typeof setLoaderAC>
+type SetErrorActionType = ReturnType<typeof setErrorActionAC>
+export type InitialStateType = typeof initialState
+type ActionsType = SetSuccessMessageActionType | LoaderActionType | SetErrorActionType
--- src/tests/registration-page-reducer.test.ts
+++ src/tests/registration-page-reducer.test.ts
@@ -1,3 +1,39 @@
-import React from "react";
+import {
+    InitialStateType,
+    registrationPageReducer,
+    setErrorActionAC, setLoaderAC,
+    setSuccessMessageAC
+} from "../BLL/registrationPageReducer";
 
-test('first test', () => {});
+let startState: InitialStateType
+beforeEach(() => {
+    startState = {
+        successMessage: false,
+        loader: false,
+        error: ''
+    }
+})
+
+test('correct error message should be set', () => {
+    const action = setErrorActionAC('Error!!!')
+    const endAppState = registrationPageReducer(startState, action)
+    expect(endAppState.error).toBe('Error!!!')
+    expect(endAppState.loader).toBe(false)
+    expect(endAppState.successMessage).toBe(false)
+});
+
+test('correct successMessage should be set', () => {
+    const action = setSuccessMessageAC(true)
+    const endAppState = registrationPageReducer(startState, action)
+    expect(endAppState.error).toBe('')
+    expect(endAppState.loader).toBe(false)
+    expect(endAppState.successMessage).toBe(true)
+});
+
+test('correct loader should be set', () => {
+    const action = setLoaderAC(true)
+    const endAppState = registrationPageReducer(startState, action)
+    expect(endAppState.error).toBe('')
+    expect(endAppState.loader).toBe(true)
+    expect(endAppState.successMessage).toBe(false)
+});
