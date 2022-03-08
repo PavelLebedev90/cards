0720b89a9a577ab43f418170260fda7cd73e6da5
Dima12411 <fixer40delta@gmail.com>
2022-03-08 23:29:00 +0300

    create auth-api

---
+++ src/api/auth-api.ts
@@ -0,0 +1,36 @@
+import axios, {AxiosResponse} from "axios";
+
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    withCredentials: true
+})
+
+export const authAPI = {
+    updateEmail(userId: string, model:UpdateUserModelType) {
+        return instance.put<UpdateUserModelType, AxiosResponse<ResponseType>>(`auth/me/${userId}`, model)
+    }
+}
+
+type ResponseType = {
+    addedUser: AddedUserType
+    error?: string
+}
+
+type AddedUserType = {
+    created: string
+    email: string
+    isAdmin: boolean
+    name: string
+    publicCardPacksCount: number
+    rememberMe: boolean
+    updated: string
+    verified: boolean
+    __v: number
+    _id: string
+}
+
+type UpdateUserModelType = {
+    name: string
+    avatar: string
+}
