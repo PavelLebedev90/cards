ba9ff58831fbe6c2e07d6ed81ec54891c220c00d
Dima12411 <fixer40delta@gmail.com>
2022-03-10 19:39:43 +0300

    create action creator and typing

--- src/BLL/profileReducer.ts
+++ src/BLL/profileReducer.ts
@@ -1,13 +1,31 @@
-const initialState = {}
+import {Dispatch} from "redux";
+import {authAPI, UpdateUserModelType} from "../api/auth-api";
+
+const initialState = {
+
+}
 
 
-export const profileReducer = (state: ProfileInitialStateType, action: any): ProfileInitialStateType => {
+export const profileReducer = (state: ProfileInitialStateType = initialState, action: AllProfileActionsCreatorsType): ProfileInitialStateType => {
     switch(action.type) {
-
-
+        case "profile/USER_RENAME":
+        return {...state, name: action.name}
         default:
             return state
     }
 }
 
+//action creators
+const userRenameAC = (name: string) => ({type: 'profile/USER_RENAME', name})
+
+
+
+//thunks
+
+
+
+
+//types
+type UserRenameType = ReturnType<typeof userRenameAC>
 export type ProfileInitialStateType = typeof initialState
+type AllProfileActionsCreatorsType = UserRenameType
