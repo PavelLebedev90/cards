cc9451b44aaef0696c3d7a807baff14ad53b476a
Dima12411 <fixer40delta@gmail.com>
2022-03-11 17:21:43 +0300

    use thunk

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import styles from './Profile.module.css'
 import {useDispatch, useSelector} from "react-redux";
-
+import {RootStateType} from "../../BLL/store";
+import {fetchUserTC, updateUserNameTC} from "../../BLL/profileReducer";
+import {useNavigate} from 'react-router-dom';
+import Preloader from "../../features/Preloader";
 
 
 const Profile = () => {
+    const loader = useSelector<RootStateType, boolean>(state => state.profile.loader)
+    const [nameState, setName] = useState<string>('')
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+            dispatch(fetchUserTC())
+    }, [])
+
+    const nicknameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+
+    const onClickHandler = () => {
+        dispatch(updateUserNameTC({
+            name: nameState
+        }))
+    }
 
     return (
         <div className={styles.mainBlock}>
@@ -16,12 +36,15 @@ const Profile = () => {
                 </div>
                 <div className={styles.blockForm}>
                     <div className={styles.inputs}>
-                        <input className={styles.input} type="text" placeholder='Nickname' value={''}/>
-                        <input className={styles.input} type="text" placeholder='Email'/>
+                        <input className={styles.input} type="text" placeholder='Nickname' onChange={nicknameHandler}/>
+                        <input className={styles.input} type="text" placeholder='Email' value={''}/>
                     </div>
                     <div className={styles.buttons}>
-                        <button className={`${styles.button} ${styles.cancelButton}`}>Cancel</button>
-                        <button className={`${styles.button} ${styles.saveButton}`}>Save</button>
+                            <button className={`${styles.button} ${styles.cancelButton}`}>Cancel</button>
+                        {loader
+                            ? <Preloader/>
+                            : <button className={`${styles.button} ${styles.saveButton}`} onClick={onClickHandler}>Save</button>
+                        }
                     </div>
                 </div>
             </div>
