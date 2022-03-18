1079c3276624e3d8fd220ba15374c5a0a2a69974
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-18 17:07:47 +0300

    add cards api, and reducer with action creators

--- src/BLL/cardsReducer.ts
+++ src/BLL/cardsReducer.ts
@@ -1,9 +1,88 @@
+import {CardFetchDataType, CardsDataType, CardType} from "../api/card-api";
 
-const initialState = {}
+const initialState: InitialStateType = {
+    cards: {
+        cards: [] as Array<CardType>,
+        cardsTotalCount: 0,
+        maxGrade: 0,
+        minGrade: 0,
+        page: 0,
+        pageCount: 0,
+        packUserId: '',
+    },
+    cardsFetchData: {
+        cardAnswer: '',
+        cardQuestion: '',
+        cardsPack_id: '',
+        min: 0,
+        max: 0,
+        sortCards: '0grade',
+        page: 0,
+        pageCount: 50,
+    },
+    error: '',
+}
 
-export const cardsReducer = (state=initialState, action: any): any =>{
-    switch (action.type){
+export const cardsReducer = (state = initialState, action: ActionsCardsType): InitialStateType => {
+    switch (action.type) {
+        case "CARDS/SET-CARDS":
+            return {
+                ...state,
+                cards: {
+                    ...state.cards,
+                    ...action.payload.cards
+                }
+            }
+        case "CARDS/SET-CARDS-FETCH-DATA":
+            return {
+                ...state,
+                cardsFetchData: {
+                    ...state.cardsFetchData,
+                    ...action.payload.cardsFetchData
+                }
+            }
+        case "CARDS/SET-ERROR":
+            return {
+                ...state,
+                ...action.payload
+            }
         default:
             return state
     }
+}
+
+//actions
+
+export const setCards = (cards: CardsDataType) => {
+    return {
+        type: 'CARDS/SET-CARDS',
+        payload: {cards}
+    } as const
+}
+
+export const setError = (error: string) => {
+    return {
+        type: 'CARDS/SET-ERROR',
+        payload: {error}
+    } as const
+}
+
+export const setPacksFetchData = (cardsFetchData: CardFetchDataType) => {
+    return {
+        type: 'CARDS/SET-CARDS-FETCH-DATA',
+        payload: {cardsFetchData}
+    } as const
+}
+
+//types
+type ActionsCardsType =
+    ReturnType<typeof setCards>
+    | ReturnType<typeof setPacksFetchData>
+    | ReturnType<typeof setError>
+
+
+type InitialStateType = {
+    cards: CardsDataType
+    cardsFetchData: CardFetchDataType
+    error: string
 }
--- src/api/card-api.ts
+++ src/api/card-api.ts
@@ -1,1 +1,49 @@
 import axios from 'axios';
+
+export const instanceCard = axios.create({
+    baseURL:
+    // 'https://neko-back.herokuapp.com/2.0'
+        process.env.REACT_APP_BACK_URL
+        || 'http://localhost:7542/2.0/',
+    withCredentials: true,
+})
+
+export const cardsApi = {
+    getCards(cardsFetchData: CardFetchDataType = {} as CardFetchDataType) {
+        return instanceCard.get<CardsDataType>(`/cards/card`, {params: cardsFetchData})
+    }
+}
+
+
+export type CardFetchDataType = {
+    cardAnswer: string
+    cardQuestion: string
+    cardsPack_id: string
+    min: number
+    max: number
+    sortCards: string
+    page: number
+    pageCount: number
+}
+
+export type CardType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: Date
+    updated: Date
+    _id: string
+}
+
+export type CardsDataType =  {
+    cards: Array<CardType>
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
