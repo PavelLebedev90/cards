aaf41a23306dc07db011314c90861367f3605390
Pavel Lebedev <pashoktver90@gmail.com>
2022-03-22 13:31:41 +0300

    commit

---
+++ src/api/card-api.ts
@@ -0,0 +1,66 @@
+import axios from 'axios';
+
+export const instanceCard = axios.create({
+    baseURL:
+    // 'https://neko-back.herokuapp.com/2.0'
+        process.env.REACT_APP_BACK_URL
+        || 'http://localhost:7542/2.0/',
+    withCredentials: true,
+})
+
+export const cardsApi = {
+    getCards(cardsFetchData: CardFetchDataType = {} as CardFetchDataType) {
+        return instanceCard.get<CardsDataType>(`/cards/card`, {params: cardsFetchData})
+    },
+    postNewCard(newCardData: NewCardDataType) {
+        return instanceCard.post<CardsDataType>(`/cards/card`, {card: newCardData})
+    },
+    deleteCard(idCard: string) {
+        return instanceCard.delete(`/cards/card?id=${idCard}`)
+    }
+}
+
+export type NewCardDataType = {
+    cardsPack_id: string
+    question: string // если не отправить будет таким
+    answer: string// если не отправить будет таким
+    grade: number // 0..5, не обязателен
+    shots: number // не обязателен
+    answerImg: string // не обязателен
+    questionImg: string // не обязателен
+    questionVideo: string // не обязателен
+    answerVideo: string // не обязателен
+}
+
+export type CardFetchDataType = {
+    cardAnswer: string
+    cardQuestion: string
+    cardsPack_id: string
+    min: number
+    max: number
+    sortCards: string
+    page: number
+    pageCount: number
+}
+
+export type CardType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: Date
+    updated: Date
+    _id: string
+}
+
+export type CardsDataType =  {
+    cards: Array<CardType>
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
