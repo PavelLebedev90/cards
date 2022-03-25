b9d10dc32ce32e3229c19fefe8a3cda0139d1efa
Pavel Lebedev <pashoktver90@gmail.com>
2022-03-25 13:42:17 +0300

    api for github-pages

--- src/api/profile-api.ts
+++ src/api/profile-api.ts
@@ -3,9 +3,9 @@ import axios, {AxiosResponse} from "axios";
 
 const instance = axios.create({
     baseURL:
-    // 'https://neko-back.herokuapp.com/2.0',
-        process.env.REACT_APP_BACK_URL
-        || 'http://localhost:7542/2.0/',
+        'https://neko-back.herokuapp.com/2.0',
+    // process.env.REACT_APP_BACK_URL
+    // || 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
 
