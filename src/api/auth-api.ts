0ea111ae903977164bb6ed2779b31f7c31a02876
Pavel Lebedev <pashoktver90@gmail.com>
2022-03-25 13:44:41 +0300

    api for local host

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -5,9 +5,9 @@ import {InitialDataType} from './login-api';
 
 export const instanceLogin = axios.create({
     baseURL:
-        'https://neko-back.herokuapp.com/2.0',
-    // process.env.REACT_APP_BACK_URL
-    // || 'http://localhost:7542/2.0/',
+        // 'https://neko-back.herokuapp.com/2.0',
+    process.env.REACT_APP_BACK_URL
+    || 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
 
--- src/api/card-api.ts
+++ src/api/card-api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const instanceCard = axios.create({
     baseURL:
-        'https://neko-back.herokuapp.com/2.0',
-    // process.env.REACT_APP_BACK_URL
-    // || 'http://localhost:7542/2.0/',
+    // 'https://neko-back.herokuapp.com/2.0',
+        process.env.REACT_APP_BACK_URL
+        || 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
 
