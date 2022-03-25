38ee408f84cbb7e40eb836c506080c58751dee60
Pavel Lebedev <pashoktver90@gmail.com>
2022-03-25 13:33:40 +0300

    modals

--- src/components/CardList/ActionBlock/ActionBlock.tsx
+++ src/components/CardList/ActionBlock/ActionBlock.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import stylesCardsList from "../CardList.module.css";
+import {ModalCRUDType} from '../../PacksList/TablePacks/TablePacks';
 
 type ActionBlockType = {
     isAnotherUser: boolean
     idCard: string
     isLoading: boolean
-    deleteCardHandler: (id: string) => void
+    opening: (modal: ModalCRUDType, idCard?:string) => void
 }
 
 const ActionBlock: React.FC<ActionBlockType> = ({
                                                     isLoading,
-                                                    deleteCardHandler,
+                                                    opening,
                                                     idCard,
                                                     isAnotherUser
                                                 }) => {
-    const callbackDeleteButton = () => {
-        deleteCardHandler(idCard)
-    }
+
     return (
         <div>
             <button className={`${stylesCardsList.tableButton}`}
-                    disabled={isLoading || isAnotherUser}>update
+                    disabled={isLoading || isAnotherUser}
+                    onClick={()=>opening('changeCard', idCard)}
+            >edit
             </button>
             <button
-                onClick={callbackDeleteButton}
+                onClick={()=>opening('deleteCard', idCard)}
                 className={`${stylesCardsList.tableButton} ${stylesCardsList.deleteTableButton}`}
                 disabled={isLoading || isAnotherUser}>delete
             </button>
